b2e74a0dc9e9e4f3f36108e90f54c05806e34328
unknown <stanley.chen021@gmail.com>
2020-05-06 20:05:09 -0400

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+console.log("clientside java")
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector("input")
+const message = document.querySelector('#mesage-1')
+const message1 = document.querySelector('#message-2')
+
+
+
+
+weatherForm.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    message.textContent = 'loading....'
+    message1.textContent = ""
+    const location = search.value
+    console.log(location)
+
+    fetch("http://localhost:3000/weather?address="+location).then((response) => {
+    response.json().then((data)=>{
+        if (data.error){
+            return message.textContent= data.error
+        }
+        else{
+            message.textContent = data.location
+            message1.textContent = data.forecast
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
+
+})
---
+++ src/app.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const path = require("path")
+const hbs = require('hbs')
+const geocode = require("./utils/geocode")
+const forecast = require("./utils/forecast")
+
+const app = express()
+const htmlpath = path.join(__dirname, "../public")
+const viewspath = path.join(__dirname, "../templates/views")
+const partialspath = path.join(__dirname, "../templates/partials")
+
+app.set("view engine","hbs")
+app.set('views', viewspath)
+hbs.registerPartials(partialspath)
+app.use(express.static(htmlpath))
+
+app.get('', (req, res) => {
+    res.render('index',{
+        title: "Weather",
+        name: "stanley chen"
+
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('About',{
+        title: "About",
+        name: "stanley chen"
+
+    })
+})
+app.get('/help', (req, res) => {
+    res.render('Help',{
+        title: "Help",
+        message: "Life can suck but, you should neever give up even if you want to.",
+        name: "Stanley Chen"
+
+    })
+})
+app.get("/weather",(req,res) =>{
+    if (!req.query.address){
+        return res.send({
+            error: "You did not search for an address"
+        })
+    }
+    geocode(req.query.address,(error,{latitude,longitude,location} = {})=>{
+        if (error){
+            return res.send({
+                error
+            })
+        }
+
+        forecast(latitude,longitude,(error,forecastData)=>{
+            if (error){
+                return res.send({error})
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+            console.log(forecastData)
+    
+        })
+    })
+})
+
+
+app.get('/help/*' , (req,res)=> {
+    res.render('404',{
+        title: "404",
+        error: "help article not found",
+        name: "stanley chen"
+    })
+    
+})
+
+app.get('*', (req,res)=>{
+    res.render('404',{
+        title: "404",
+        error: "Page not found",
+        name: "stanley chen"
+    })
+})
+
+
+
+
+
+app.listen(3000, ()=>{
+
+    console.log('server is up on port 3000')
+
+
+})
