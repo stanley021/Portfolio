bd2389c79afa9ea01eef3e1c044ae5a5ca75c03f
unknown <stanley.chen021@gmail.com>
2020-05-26 01:16:11 +0800

    Adjusted the correct units for celsius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/f1e88124533815db25c56db3bc5253e8/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/f1e88124533815db25c56db3bc5253e8/' + latitude + ',' + longitude + "?units=si"
     
 
     request({ url, json: true }, (error, {body}) => {
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             console.log(body.daily.data[0])
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. The humidity is currently '+ body.daily.data[0].humidity+ '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' celsius outside. The humidity is currently '+ body.daily.data[0].humidity+ '. There is a ' + body.currently.precipProbability + '% chance of rain. The minimum temperature is ' + body.daily.data[0].apparentTemperatureMin + " celsius with a maximum temperature of " + body.daily.data[0].apparentTemperatureMax+" celsius.")
             
         }
     })
